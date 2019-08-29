23434f4d3f9bb322dbf08712d5709db11244a4ff
Lee Robinson <lrobinson2011@gmail.com>
2019-08-28 20:03:16 -0500

    chore: Update all deps and use new ESLint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    extends: 'get-off-my-lawn-prettier',
+    extends: 'get-off-my-lawn',
     rules: {
-        'node/no-extraneous-import': '',
-        'import/no-extraneous-dependencies': 'off',
-        'import/no-unresolved': 'off'
+        'node/no-extraneous-import': 0,
+        'import/no-extraneous-dependencies': 0,
+        'unicorn/filename-case': 0
     }
 };
