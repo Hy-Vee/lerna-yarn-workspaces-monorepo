493c9010de0f6deb70b437eaa6511228e90cac41
Lee Robinson <lrobinson2011@gmail.com>
2019-08-30 09:46:11 -0500

    fix: Ugrade eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     extends: 'get-off-my-lawn',
     rules: {
         'node/no-extraneous-import': 0,
-        'import/no-extraneous-dependencies': 0,
-        'unicorn/filename-case': 0
+        'import/no-extraneous-dependencies': 0
     }
 };
