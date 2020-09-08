a6872264d338d3263ab9246ada2933af28f85e1d
Lee Robinson <lrobinson2011@gmail.com>
2020-09-08 11:36:57 -0500

    Update dependencies.

--- packages/input/src/Input.stories.js
+++ packages/input/src/Input.stories.js
@@ -4,7 +4,7 @@ import Input from '.';
 
 export default {
     component: Input,
-    title: 'Design System|Input'
+    title: 'Design System/Input'
 };
 
 export const placeholder = () => <Input id="email" label="Email" placeholder="user@gmail.com" />;
