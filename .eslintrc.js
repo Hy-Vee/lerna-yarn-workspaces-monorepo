02c9ce1b700d73ac1faacb67a41bd3b64619876d
Lee Robinson <lrobinson2011@gmail.com>
2020-09-08 11:39:46 -0500

    More deps.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: 'get-off-my-lawn',
     rules: {
-        'node/no-extraneous-import': 0,
-        'import/no-extraneous-dependencies': 0
+        'import/no-extraneous-dependencies': 0,
+        'node/no-extraneous-import': 0
     }
 };
