7fba373b38e87c5f4a9e79edc773c3bee703cebe
Molly Moran <mmoran@hy-vee.com>
2021-06-26 13:49:43 -0500

    Updates the project name and exports

---
+++ packages/design-system-styled-components/src/index.js
@@ -0,0 +1,6 @@
+import Input from './input/src';
+import Textarea from './textarea/src';
+import {spacing, H1, H2, P} from './theme/src';
+import Button from './button/src';
+
+export {Button, Input, Textarea, spacing, H1, H2, P};
