3c4265cfa8291e6927c976b55fdf5b9d6f0032ef
Molly Moran <mmoran@hy-vee.com>
2021-06-27 16:21:21 -0500

    Adds textarea and compiles sass.

---
+++ packages/design-system-sass/src/components/input/Input.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Input from '.';
+
+export default {
+    component: Input,
+    title: 'Sass Design System/Input'
+};
+
+export const placeholder = () => <Input id="email" label="Email" placeholder="user@gmail.com" />;
+
+export const disabled = () => <Input disabled id="email" label="Email" placeholder="user@gmail.com" />;
